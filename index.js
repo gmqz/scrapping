be61c9eaa9219d9989e4555904d4955b0e863e76
Ubuntu <ubuntu@ip-172-31-1-221.ec2.internal>
2017-05-19 19:03:04 +0000

    First test

---
+++ index.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+app.get('/', function(req, res){
+    res.send('Hi, Check your console!')
+})
+
+app.get('/scrape', function(req, res){
+   //All the web scraping magic will happen here
+	sites = [
+                {
+                    url: 'https://www.icefrance.fr/en',
+                    getData : function($){
+                		let res=[];
+                		$('#simply-scroll-products_ice-block_home ul').children().map(function(ele){
+                                res.push($(this).text());
+                        })
+                		return res;
+                	}
+
+                },{
+                    url: 'http://www.bancopiano.com.ar/Cotizaciones/Default',
+                	getData: function($){
+                        let res=[];
+                		$tr = $('.tabla-cotizaciones').find('img[title="USD"]').parent().parent();
+                        res.push($tr.children().eq(1).text());
+                        $tr = $('.tabla-cotizaciones').find('img[title="EUR"]').parent().parent();
+                        res.push($tr.children().eq(1).text());
+                		return res;
+                    }
+                },{
+                    url: 'http://www.fvaccaro.comm/',
+                	getData: function($){return [];}
+                }
+            ],
+    round = [];
+
+    sites.forEach(function(site){
+        let ini = new Date();
+        var json = {
+            site: "",
+            status: "",
+            date: "",
+            info: [],
+            queryTime: ""
+        };
+        json.date = new Date();
+        json.site = site.url;
+        request(site.url, (error, response, html)=>{
+            if(!error) {
+                var $ = cheerio.load(html);
+                json.status = 'success';
+                json.info = site.getData($);
+            }else{
+                json.status = 'error';
+                console.log('Request Error')
+            }
+            let fin = new Date();
+            json.queryTime = fin - ini;
+            round.push(json);
+            console.log(round);
+        });
+    })
+    //console.log(round);
+    res.send('Check your console!');
+})
+
+app.listen('8081')
+console.log('Magic happens on port 8081');
+exports = module.exports = app;
